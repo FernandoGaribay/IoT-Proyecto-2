575eb57c782b4c77963f50948630480977c4e0e0
FernandoGaribay <garibaycejaf@gmail.com>
2024-10-18 18:22:24 -0600

    + message_box.py -> imports updated

--- helpers/message_box.py
+++ helpers/message_box.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QApplication, QMainWindow, QMessageBox, QPushButton, QVBoxLayout, QWidget
+from PySide6.QtWidgets import QMessageBox
 
 def show_alert(parent):
     QMessageBox.warning(parent, "Warning", "Username and password cannot be empty.")
